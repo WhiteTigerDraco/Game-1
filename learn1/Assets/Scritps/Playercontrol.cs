7e92c333e511841af1226f32f37e2b2caff7db4d
WhiteTigerDraco <33595192+WhiteTigerDraco@users.noreply.github.com>
2018-02-12 01:49:02 +0530

    Initial part 01
    Learning phase 01

---
+++ learn1/Assets/Scritps/Playercontrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrol : MonoBehaviour {
+    public int score = 0;
+    private float movement=0f;
+    public float jump = 7f;
+        public float speed=5f;
+    public Transform groundCheckpoint;
+    public float groundradius;
+    public LayerMask  groundlayermask;
+    public bool isgroundTouch;
+    private Rigidbody2D rigidBody;
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isgroundTouch = Physics2D.OverlapCircle(groundCheckpoint.position, groundradius, groundlayermask);
+        movement = Input.GetAxis("Horizontal");
+        if (movement != 0f)
+        {
+            rigidBody.velocity = new Vector2 (movement *speed, rigidBody.velocity.y);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y);
+        }
+       
+        if (Input.GetButtonDown("Jump")  && isgroundTouch  )
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jump);
+        }
+		
+	}
+}
---
+++ learn1/Assets/Scritps/coin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour {
+    Playercontrol a;
+        // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("tril");
+        Destroy(gameObject);
+      // a.score+=1;
+        Debug.Log("score: " );
+    }
+}
